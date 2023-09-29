66d08d98bdfb25d8eaf9a36260b3f32440a569be
louxuyang <louxuyang7@gmail.com>
2023-09-29 12:41:54 +0900

    up

--- 04. Data_Graph(3).R
+++ 04. Data_Graph(3).R
@@ -1,29 +1,29 @@
 #table()함수/구분 1개의 인자를 가지고 도수분포표 작성
-table(X2023_STB_survey $Gender)
+table(X2023_STB_survey $성별)
 
 #상대도수 계산
-ECN <- table(X2023_STB_survey $Gender)
+ECN <- table(X2023_STB_survey $성별)
 prop.table(ECN)
 
 #table()함수/2개의 인자를 가지고 교차표를 작성
-table(X2023_STB_survey $Gender, X2023_STB_survey $Grade)
+table(X2023_STB_survey $성별, X2023_STB_survey $학년)
 
 #막대그래프
-barplot(table(X2023_STB_survey $Nationality))
+barplot(table(X2023_STB_survey $국적))
 
 barplot(table(X2023_STB_survey $주택가),horiz=TRUE)
 
-entry <- table(X2023_STB_survey $Gender, X2023_STB_survey $Grade)
+entry <- table(X2023_STB_survey $성별, X2023_STB_survey $학년)
 barplot(entry, legend = TRUE)
 
 #파이차트
-pie(table(X2023_STB_survey $Grade))
+pie(table(X2023_STB_survey $학년))
 
 #히스토그램
-hist(X2023_STB_survey$`Age`, main="2023년 경영통계분석2를 수강하는 수강생들의 나이",col=terrain.colors(12))
+hist(X2023_STB_survey$`연세`, main="2023년 경영통계분석2를 수강하는 수강생들의 연세",col=terrain.colors(12))
 
 #박스 플롯
-boxplot(X2023_STB_survey$`Grade`, X2023_STB_survey$`Age`,main="Grade별 Age에 대한 기술통계분석", col="yellow", names =c("Grade","Age"))
+boxplot(X2023_STB_survey$`학년`, X2023_STB_survey$`연세`,main="Grade별 연세에 대한 기술통계분석", col="yellow", names =c("학년","연세"))
 
 #삱점도
-plot(x=X2023_STB_survey$`Grade`,  y=X2023_STB_survey$`Age`,  xlab="Grade", ylab="Age", main="Gread와 Age의 변화")
+plot(x=X2023_STB_survey$`학년`,  y=X2023_STB_survey$`연세`,  xlab="학년", ylab="연세", main="학년와 연세의 변화")
