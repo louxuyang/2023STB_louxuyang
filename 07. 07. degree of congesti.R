568ecde2e0f1d9db6aeb4de7ef15359a34905487
louxuyang <louxuyang7@gmail.com>
2023-10-30 11:20:36 +0900

    up

--- 07. 07. degree of congesti.R
+++ 07. 07. degree of congesti.R
@@ -32,15 +32,15 @@ ggplot(congestion1, aes(y=s0530))+
 
 summary(congestion1$s0530)
 
-#승차승객수가가장많았던역의노선을찾아보기
+#수도권 지하철 혼잡도를분석
 #1.지하철역의 하루 평균 혼잡도
 congestion1$day_mean <-
   rowMeans(congestion1[,c('s0530','s0600','s0630','s0700','s0730','s0800','s0830','s0900','s0930','s1000','s1030','s1100','s1130','s1200','s1230','s1300','s1330','s1400','s1430','s1500','s1530','s1600','s1630','s1700','s1730','s1800','s1830','s1900','s1930','s2000','s2030','s2100','s2130','s2200','s2230','s2300','s2330')])
+
 mean(congestion1$day_mean)
 
 #2.지하철 호선별 하루 평균 혼잡도
 max(congestion1$day_mean)
-
 congestion1%>% filter(day_mean==60.1)%>%  
   select(type, line, station,way)
 
@@ -65,6 +65,7 @@ head(passenger10, 8)
 ggplot(data=passenger10, aes(x=reorder(line, m), y=m))+
   geom_col()+
   coord_flip()
+
 #3-2.지하철 호선별 7시30의 평균혼잡도
 passenger10 <-congestion1 %>%
   group_by(line)%>%
@@ -76,6 +77,7 @@ head(passenger10, 8)
 ggplot(data=passenger10, aes(x=reorder(line, m), y=m))+
   geom_col()+
   coord_flip()
+
 #3-3.지하철 호선별 8시의 평균혼잡도
 passenger10 <-congestion1 %>%
   group_by(line)%>%
@@ -87,6 +89,7 @@ head(passenger10, 8)
 ggplot(data=passenger10, aes(x=reorder(line, m), y=m))+
   geom_col()+
   coord_flip()
+
 #3-4.지하철 호선별 8시30의 평균혼잡도
 passenger10 <-congestion1 %>%
   group_by(line)%>%
@@ -98,6 +101,7 @@ head(passenger10, 8)
 ggplot(data=passenger10, aes(x=reorder(line, m), y=m))+
   geom_col()+
   coord_flip()
+
 #3-5.지하철 호선별 9시의 평균혼잡도
 passenger10 <-congestion1 %>%
   group_by(line)%>%
@@ -109,7 +113,8 @@ head(passenger10, 8)
 ggplot(data=passenger10, aes(x=reorder(line, m), y=m))+
   geom_col()+
   coord_flip()
-#3-6
+
+#3-6평균혼잡도 상위 4개 호선의 역별 기여도
 congestion1%>%
   group_by(line) %>%  
   summarise(total=sum(day_mean)) %>%
@@ -125,6 +130,7 @@ mutate(s80_grade=ifelse(s0800<=80, "good", ifelse(s0800<=130, "normal", ifelse(s
   mutate(total=sum(n), pct=round(n/total*100,1))%>%  
   select(s80_grade,n,pct)%>%  
   arrange(desc(n))
+
 #4-2.호선별로 08시 지하철 혼잡도 범주화
 congestion1 %>%  
   mutate(s80_grade=ifelse(s0800<=80, "good", ifelse(s0800<=130, "normal", ifelse(s0800<=150, "caution", "bad"))))%>%
@@ -148,6 +154,7 @@ head(passenger10, 8)
 ggplot(data=passenger10, aes(x=reorder(line, m), y=m))+
   geom_col()+
   coord_flip()
+
 #5-2.지하철 호선별 18시30의 평균혼잡도
 passenger10 <-congestion1 %>%
   group_by(line)%>%
@@ -159,6 +166,7 @@ head(passenger10, 8)
 ggplot(data=passenger10, aes(x=reorder(line, m), y=m))+
   geom_col()+
   coord_flip()
+
 #5-3.지하철 호선별 19시의 평균혼잡도
 passenger10 <-congestion1 %>%
   group_by(line)%>%
@@ -170,6 +178,7 @@ head(passenger10, 8)
 ggplot(data=passenger10, aes(x=reorder(line, m), y=m))+
   geom_col()+
   coord_flip()
+
 #5-4.지하철 호선별 19시30의 평균혼잡도
 passenger10 <-congestion1 %>%
   group_by(line)%>%
@@ -181,6 +190,7 @@ head(passenger10, 8)
 ggplot(data=passenger10, aes(x=reorder(line, m), y=m))+
   geom_col()+
   coord_flip()
+
 #5-5.지하철 호선별 20시의 평균혼잡도
 passenger10 <-congestion1 %>%
   group_by(line)%>%
@@ -192,7 +202,8 @@ head(passenger10, 8)
 ggplot(data=passenger10, aes(x=reorder(line, m), y=m))+
   geom_col()+
   coord_flip()
-#5-6
+
+#5-6평균혼잡도 상위 4개 호선의 역별 기여도
 congestion1%>%
   group_by(line) %>%  
   summarise(total=sum(day_mean)) %>%
@@ -208,6 +219,7 @@ congestion1 %>%
   mutate(total=sum(n), pct=round(n/total*100,1))%>%  
   select(s80_grade,n,pct)%>%  
   arrange(desc(n))
+
 #6-2.호선별로 18시 지하철 혼잡도 범주화
 congestion1 %>%  
   mutate(s80_grade=ifelse(s0800<=80, "good", ifelse(s0800<=130, "normal", ifelse(s0800<=150, "caution", "bad"))))%>%
